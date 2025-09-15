77e14de6fc8b1be507d23ef3f9eaebbaece1460b
Kamil Kanngo <kamil.kanngo@elev.ga.ntig.se>
2025-09-15 12:46:30 +0200

    first commit

---
+++ tamagotchi/Class1.cs
@@ -0,0 +1,38 @@
+public class Tamagotchi
+{
+    private int Hunger;
+    private int Boredom;
+    public string Name;
+    private bool IsAlive;
+    private List<string> words;
+
+    public void Feed()
+    {
+
+    }
+
+    public void Hi()
+    {
+
+    }
+
+    public bool GetAlive()
+    {
+
+    }
+
+    public void Tick()
+    {
+
+    }
+
+    public void PrintStats()
+    {
+        Console.WriteLine($"Name: {Name} || Hunger: {Hunger} || Boredom: {Boredom} || Status: {isAlive} || Ord: {words.Count}");
+    }
+
+    private void ReduceBoredom()
+    {
+
+    }
+}
---
+++ tamagotchi/Program.cs
@@ -0,0 +1,18 @@
+﻿Tamagotchi tami = new();
+
+tami.PrintStats();
+
+Console.WriteLine("");
+string Name = Console.ReadLine();
+Console.WriteLine($"{Name} är ett underbart namn!");
+Console.ReadLine();
+Console.Clear();
+
+Console.WriteLine("");
+while (TrueOrFalse == false)
+{
+    Console.WriteLine("");
+    Console.WriteLine("|| Nytt ord || Hälsa || Mata || Gör inget ||");
+    string S = Console.ReadLine();
+
+}
